31df6b1a055e1f3924a22d190e31ff1589613853
lebron-embiid <973265025@qq.com>
2019-05-25 18:07:37 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,71 +1,96 @@
 <template>
 	<view class="index_view">
 		<video id="myVideo" :src="url" :poster="poster_img" :loop="play" :show-fullscreen-btn="progress" :autoplay="autoplay" :show-center-play-btn="play" :enable-progress-gesture="progress" :controls="controls" direction="0">
-			
-		</video>
-		<cover-view class="cover_video_view">
 			<cover-view class="cv_title">20/16</cover-view>
-			<cover-view class="operate_box">
-				<cover-view class="ob_avatar"><cover-image src="../../static/img/index_avatar.png"></cover-image></cover-view>
-				<cover-view class="ob_item ob_collect"><cover-image src="../../static/img/index_icon1.png"></cover-image><cover-view>566w</cover-view></cover-view>
-				<cover-view class="ob_item ob_love"><cover-image src="../../static/img/index_icon2.png"></cover-image><cover-view>13.8w</cover-view></cover-view>
-				<cover-view class="ob_item ob_commont"><cover-image src="../../static/img/index_icon3.png"></cover-image><cover-view>350</cover-view></cover-view>
-				<cover-view class="ob_item ob_share"><cover-image src="../../static/img/index_icon4.png"></cover-image><cover-view>1.2w</cover-view></cover-view>
+			<cover-view class="ob_avatar_border"></cover-view>
+			<cover-image class="ob_avatar" src="../../static/img/index_avatar.png"></cover-image>
+			<cover-image class="ob_item oc_img1" src="../../static/img/index_icon1.png"></cover-image><cover-view class="ob_txt ob_collect">566w</cover-view>
+			<cover-image class="ob_item oc_img2" src="../../static/img/index_icon2.png"></cover-image><cover-view class="ob_txt ob_love">13.8w</cover-view>
+			<cover-image class="ob_item oc_img3" src="../../static/img/index_icon3.png"></cover-image><cover-view class="ob_txt ob_comment">350</cover-view>
+			<cover-image class="ob_item oc_img4" src="../../static/img/index_icon4.png"></cover-image><cover-view class="ob_txt ob_share">1.2w</cover-view>
+			<cover-image class="cs_left" src="../../static/img/theme_icon.png"></cover-image>
+			<cover-view class="cs_right">新惠设计主流款...</cover-view>
+			<cover-view class="cover_word">
+				高品质制造平台,三体系认证,十环认证 平台,三体系认证十环认证平台,三体系认证,十环认证高品质制造
 			</cover-view>
-			<cover-view class="cover_content">
-				<cover-view class="cover_style">
-					<cover-view class="cs_left"><cover-image src="../../static/img/theme_icon.png"></cover-image></cover-view>
-					<cover-view class="cs_right">新惠设计主流款...</cover-view>
-				</cover-view>
-				<cover-view class="cover_word">
-					高品质制造平台,三体系认证,十环认证 平台,三体系认证十环认证平台,三体系认证,十环认证高品质制造
-				</cover-view>
-			</cover-view>
-		</cover-view>
+		</video>
 	</view>
 </template>
 
 <script>
+	// #ifdef APP-PLUS  
+	// var currentWebview = plus.webview.currentWebview();
+	// #endif  
 	export default{
 		data(){
 			return{
-				url: "http://vd.yinyuetai.com/sh.yinyuetai.com/uploads/videos/common/5A18014D6581072F9B3C10765252A3F1.mp4",
+				url: "http://vd.yinyuetai.com/hc.yinyuetai.com/uploads/videos/common/C55C01649BE431367709E70897C5D6F2.mp4",
 				poster_img: "../../static/img/poster.jpg",
 				controls: false,
 				autoplay: true,
 				progress: false,
-				play: true
+				play: true,
+				title: '20/16',
+				avatar: "static/img/index_avatar.png",
+				collect: "566w",
+				love: "13.8w",
+				comment: "350",
+				share: "1.2w"
 			}
 		},
 		methods:{
 			
 		},
 		onLoad() {
-			
+			var that = this;
+			// #ifdef APP-PLUS
+			// 标题部分
+			// var title = new plus.nativeObj.View('title',
+			// {top:'30px',left:'0px',height: '44px',width:'100%'});
+			// title.draw([
+			// 	{tag:'font',class:'cv_title',text: that.title,textStyles:{size:'16px',color:'#fff'}}
+			// ])
+			// // 右侧头像部分
+			// var avatar = new plus.nativeObj.View('avatar',
+			// {top:'200px',right:'0px',height: '600px',width:'100px'});
+			// avatar.draw([
+			// 	{tag:'img',src: that.avatar,position:{width:'50px',height:'47px'}},
+			// 	{tag:'font',text: that.collect,textStyles:{size:'12px',color:'#fff'}}
+			// ])
+			// 
+			// title.show();
+			// avatar.show();
+			// 
+			// if(title){
+			// 	console.log("查找到id为'test'的View控件对象");
+			// }else{
+			// 	console.log("未查找到id为'test'的View控件对象，请先创建");
+			// }
+			// #endif
 		}
 	}
 </script>
 
 <style scoped lang="scss">
-	// body,page{position: relative;width: 100%;height: 100%;}
+	body,page{position: relative;}
 	#myVideo{
-		position: relative;
 		display: block;
-		width: 100vw;
-		height: 100vh;
-		// width: 100%;
-		// height: calc(100% - 28px);
-		// left: 0;
-		// top: var(--status-bar-height);
-	}
-	.cover_video_view{
-		position: absolute;
+		// width: 100vw;
+		// height: 100vh;
 		width: 100%;
-		height: 100%;
+		height: calc(100% - 28px);
+		position: absolute;
 		left: 0;
-		top: 0;
-		padding: 30upx 20upx;
-		box-sizing: border-box;
+		top: var(--status-bar-height);
+	}
+	// .cover_video_view{
+	// 	position: absolute;
+	// 	width: 100%;
+	// 	height: 100%;
+	// 	left: 0;
+	// 	top: 0;
+	// 	// padding: 30upx 20upx;
+	// 	box-sizing: border-box;
 		// top: var(--status-bar-height);
 		.cv_title{
 			display: block;
@@ -73,80 +98,120 @@
 			color: #fff;
 			font-size: 36upx;
 			text-align: center;
-			margin: 30upx 0;
-		}
-		.operate_box{
 			position: absolute;
-			right: 20upx;
-			bottom: 210upx;
-			width: 90upx;
-			text-align: center;
-			.ob_avatar{
-				border: 2px solid #fff;
+			top: 80upx;
+		}
+		// .operate_box{
+		// 	position: absolute;
+		// 	right: 20upx;
+		// 	bottom: 210upx;
+		// 	width: 90upx;
+		// 	text-align: center;
+			.ob_avatar_border{
+				width: 90upx;
+				height: 90upx;
 				border-radius: 50%;
-				cover-image{
-					display: block;
-					width: 82upx;
-					height: 82upx;
-				}
+				background: #fff;
+				position: absolute;
+				right: 20upx;
+				bottom: 720upx;
+			}
+			.ob_avatar{
+				display: block;
+				width: 82upx;
+				height: 82upx;
+				position: absolute;
+				right: 24upx;
+				bottom: 724upx;
 			}
 			.ob_item{
-				margin-top: 40upx;
-				cover-image{
-					display: block;
-					width: 50upx;
-					height: 47upx;
-					margin: 0 auto 10upx;
-				}
-				cover-view{
-					color: #fff;
-					font-size: 24upx;
-					text-align: center;
-				}
+				display: block;
+				width: 50upx;
+				height: 47upx;
+				position: absolute;
+				right: 43upx;
 			}
-		}
-		.cover_content{
-			position: absolute;
-			left: 20upx;
-			bottom: 150upx;
-			width: 520upx;
-			.cover_style{
-				width: 270upx;
-				height: 58upx;
-				background: rgba(0,0,0,.13);
-				border-radius: 10upx;
-				display: flex;
-				justify-content: flex-start;
-				align-items: center;
-				margin-bottom: 20upx;
+			.oc_img1{
+				bottom: 640upx;
+			}
+			.oc_img2{
+				bottom: 520upx;
+			}
+			.oc_img3{
+				bottom: 400upx;
+			}
+			.oc_img4{
+				bottom: 280upx;
+			}
+			.ob_txt{
+				color: #fff;
+				font-size: 24upx;
+				text-align: center;
+				position: absolute;
+				right: 23upx;
+				width: 90upx;
+				text-align: center;
+			}
+			.ob_collect{
+				bottom: 605upx;
+			}
+			.ob_love{
+				bottom: 485upx;
+			}
+			.ob_comment{
+				bottom: 365upx;
+			}
+			.ob_share{
+				bottom: 245upx;
+			}
+		// }
+		// .cover_content{
+		// 	position: absolute;
+		// 	left: 20upx;
+		// 	bottom: 150upx;
+		// 	width: 520upx;
+			// .cover_style{
+			// 	width: 270upx;
+			// 	height: 58upx;
+			// 	background: rgba(0,0,0,.13);
+			// 	border-radius: 10upx;
+			// 	display: flex;
+			// 	justify-content: flex-start;
+			// 	align-items: center;
+			// 	margin-bottom: 20upx;
 				.cs_left{
-					width: 60upx;
-					height: 58upx;
-					background: rgba(0,0,0,.5);
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					margin-right: 10upx;
-					cover-image{
-						display: block;
-						width: 34upx;
-						height: 30upx;
-					}
+					width: 34upx;
+					height: 30upx;
+					position: absolute;
+					left: 35upx;
+					bottom: 297upx;
+					// cover-image{
+					// 	display: block;
+					// 	width: 34upx;
+					// 	height: 30upx;
+					// }
 				}
 				.cs_right{
 					color: #fff;
+					width: 200upx;
+					height: 58upx;
+					line-height: 58upx;
+					padding-left: 10upx;
 					font-size: 22upx;
-					width: 75%;
 					overflow: hidden;
 					text-overflow: ellipsis;
 					white-space: nowrap;
+					position: absolute;
+					left: 80upx;
+					bottom: 280upx;
 				}
-			}
+			// }
 			.cover_word{
 				color: #fff;
 				font-size: 22upx;
+				width: 65%;
 				height: 90upx;
-				line-height: 1.4;
+				line-height: 30upx;
 				color: #fff;
 				overflow : hidden;
 				text-overflow: ellipsis;
@@ -155,7 +220,10 @@
 				-webkit-box-orient: vertical;
 				word-wrap: break-word;
 				white-space: pre-wrap;
+				position: absolute;
+				left: 20upx;
+				bottom: 170upx;
 			}
-		}
-	}
+		// }
+	// }
 </style>
